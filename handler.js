b8dbe1d8b4df0048b002a80f45f02a087502530f
Clay Koberlein <clay@dronedeploy.com>
2018-05-21 09:56:02 -0500

    Return token to client popup window via html

--- handler.js
+++ handler.js
@@ -10,6 +10,27 @@ const oauth2InitHandler = (req, res, ctx) => {
   res.send(authorizationUri);
 };
 
+const generateCallbackHtml = (token) => {
+  token = JSON.stringify(token);
+  return '<!DOCTYPE html>'+
+  '<html lang="en">'+
+  '<head>'+
+  '    <meta charset="UTF-8">'+
+  '    <meta name="viewport" content="width=device-width, initial-scale=1.0">'+
+  '    <meta http-equiv="X-UA-Compatible" content="ie=edge">'+
+  '    <title>Document</title>'+
+  '</head>'+
+  '<body>'+
+  '    <script>'+
+  '    window.onload = function() {'+
+  `        window.opener.postMessage(\'${token}\', \'*\')`+
+  '    }'+
+  '    </script>'+
+  ''+
+  '</body>'+
+  '</html>';
+};
+
 // Callback handler to take the auth code from first OAuth2 step and get the tokens
 const oauth2CallbackHandler = (req, res, ctx) => {
   const tokenConfig = {
@@ -46,7 +67,7 @@ const oauth2CallbackHandler = (req, res, ctx) => {
             // impact potential future api calls - send error
             throw new Error(rowData.errors[0]);
           }
-          return res.status(200).send();
+          return res.status(200).send(generateCallbackHtml(result.access_token));
         });
       });
   })
