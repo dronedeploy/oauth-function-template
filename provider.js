1c89e5a1544f896ad9d7af2dd20dca9ddf0e08e7
Clay Koberlein <clay@dronedeploy.com>
2018-06-06 10:40:47 -0500

    Make template setup more generic and automatic

--- provider.js
+++ provider.js
@@ -2,9 +2,6 @@
 
 let conf = require('./oauth-config');
 
-const FUNCTION_ROOT = conf.get('authProvider');
-
-exports.functionName = FUNCTION_ROOT;
 exports.callbackUrl = conf.get('callbackUrl');
 
 exports.credentials = conf.get('credentials');
