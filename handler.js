d9b8b8a98afcdc9d09930f5f0bc8d42c5fb66519
Clay Koberlein <clay@dronedeploy.com>
2018-08-23 13:10:55 -0500

    Add body logging

--- handler.js
+++ handler.js
@@ -170,6 +170,8 @@ const oauth2CallbackHandler = (req, res, ctx) => {
 };
 
 const storeTokenHandler = (req, res, ctx) => {
+  console.log(req);
+  console.log(req.body);
   // Make sure this is called with the proper method
   if (req.method !== 'POST' && req.method !== 'PUT') {
     return res.status(400).send(createErrorHtml('Invalid request method - please use POST or PUT'));
